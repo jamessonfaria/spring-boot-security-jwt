ebe77fa21cfc7e27dea87c6031ed506620f23f33
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-18 10:53:56 -0300

    Adding api resources

---
+++ src/main/java/br/com/jamesson/api/UserResource.java
@@ -0,0 +1,24 @@
+package br.com.jamesson.api;
+
+import br.com.jamesson.domain.User;
+import br.com.jamesson.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers(){
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+}
