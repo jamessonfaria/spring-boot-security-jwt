5c992a0074b867cd50cba5712692f1749a196ef8
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-18 10:48:45 -0300

    Adjust service with logs

--- src/main/java/br/com/jamesson/service/UserServiceImpl.java
+++ src/main/java/br/com/jamesson/service/UserServiceImpl.java
@@ -21,16 +21,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to the database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -38,11 +41,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
