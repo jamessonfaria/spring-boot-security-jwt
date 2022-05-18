43792529baca4c741f3d37e0248b7adb45f55c6a
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-05-18 12:25:19 -0300

    Adding other methods to the resource

--- src/main/java/br/com/jamesson/api/UserResource.java
+++ src/main/java/br/com/jamesson/api/UserResource.java
@@ -1,13 +1,15 @@
 package br.com.jamesson.api;
 
+import br.com.jamesson.domain.Role;
 import br.com.jamesson.domain.User;
 import br.com.jamesson.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,4 +23,28 @@ public class UserResource {
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/users").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/roles")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/roles").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/roles/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form){
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
